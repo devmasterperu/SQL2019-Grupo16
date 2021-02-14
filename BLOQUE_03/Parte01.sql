d3f5ffabc48690af29b48074b51c75cb904043f8
gmanriquev <gmanrique.422@gmail.com>
2021-02-14 12:17:45 -0500

    Bloque 03

--- BLOQUE_03/Parte01.sql
+++ BLOQUE_03/Parte01.sql
@@ -42,3 +42,31 @@ inner join TipoDocumento t on c.codtipo=t.codtipo
 inner join Zona z on c.codzona=z.codzona
 where c.tipo_cliente='P' and c.estado=1
 order by NOMBRE_COMPLETO asc
+
+--03.06
+select t.tipo as TIPO,t.numero as NUMERO,t.codcliente as COD_CLIENTE,
+c.razon_social as EMPRESA,z.nombre as ZONA
+from Telefono t 
+--inner join Cliente c on t.codcliente=c.codcliente
+--inner join Zona z on c.codzona=z.codzona
+--where t.estado=1 and c.tipo_cliente='E'
+inner join Cliente c on t.codcliente=c.codcliente and t.estado=1 and c.tipo_cliente='E'
+inner join Zona z on c.codzona=z.codzona
+order by c.codcliente asc
+
+--03.08
+
+select t.tipo as TIPO,t.numero as NUMERO,
+       case 
+	   when c.tipo_cliente='P' 
+	   then concat(rtrim(ltrim(c.nombres)),' ',rtrim(ltrim(c.ape_paterno)),' ',rtrim(ltrim(c.ape_materno)))
+	   when c.tipo_cliente='E' 
+	   then c.razon_social
+	   else 'SIN DETALLE'
+	   end as CLIENTE,
+	   isnull(c.email,'SIN DETALLE') as EMAIL,
+	   coalesce(c.email,'SIN DETALLE') as EMAIL2,
+	   convert(varchar(8),getdate(),112) as FEC_CONSULTA
+from Telefono t left join Cliente c on t.codcliente=c.codcliente and t.estado=1
+where t.estado=1 --609
+order by EMAIL desc
