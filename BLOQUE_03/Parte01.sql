31fdc415601ffa66aa2a7fd5a136fdc230f6dfd5
gmanriquev <gmanrique.422@gmail.com>
2021-02-14 14:04:14 -0500

    Bloque 03

--- BLOQUE_03/Parte01.sql
+++ BLOQUE_03/Parte01.sql
@@ -93,3 +93,35 @@ p.codplan
 from PlanInternet p
 right join Contrato c on c.codplan=p.codplan
 order by p.nombre asc
+
+--03.12
+
+select isnull(c.codcliente,0) as CLIENTE_CODCLIENTE,
+	   case 
+		   when c.tipo_cliente='P' 
+		   then concat(rtrim(ltrim(c.nombres)),' ',rtrim(ltrim(c.ape_paterno)),' ',rtrim(ltrim(c.ape_materno)))
+		   when c.tipo_cliente='E' 
+		   then c.razon_social
+		   else 'SIN DATO'
+	  end as CLIENTE_NOMBRE,
+	  lower(isnull(c.email,'SIN DATO')) as CLIENTE_CORREO,
+	  isnull(co.codcliente,0) as CONTRATO_CODCLIENTE,
+	  isnull(p.nombre,'SIN DATO') as CONTRATO_PLAN,
+	  isnull(co.fec_contrato,'9999-12-31') as CONTRATO_FECHA
+from Contrato co
+full join Cliente c on co.codcliente=c.codcliente
+left join PlanInternet p on co.codplan=p.codplan
+order by isnull(co.codcliente,0) asc
+
+--Self Join
+USE TSQL
+go
+
+--Relación de empleados+jefes (independientemente de tener jefatura)
+select 
+emp.title,
+emp.firstname+' '+emp.lastname as Employee,
+jef.firstname+' '+jef.lastname as Boss
+from HR.Employees emp
+left join HR.Employees jef on emp.mgrid=jef.empid
+
